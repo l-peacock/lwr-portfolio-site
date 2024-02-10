bdd821b865049d940bbee33598951ddfe3845d78
Lachlan Peacock <lachlan.peacock@gmail.com>
2024-02-10 17:09:59 +1100

    Add unit tests

---
+++ src/modules/base/app/__tests__/app.test.js
@@ -0,0 +1,32 @@
+import { createElement } from "lwc";
+import App from "base/app";
+
+let element; // Declare an element reference which can be shared between tests.
+describe("base-app", () => {
+	beforeEach(() => {
+		element = createElement("base-app", {
+			is: App,
+		});
+	});
+
+	afterEach(() => {
+		// The jsdom instance is shared across test cases in a single file so reset the DOM
+		while (document.body.firstChild) {
+			document.body.removeChild(document.body.firstChild);
+		}
+	});
+
+	// Essentially, this test ensures that the correct semantic structure of the base application is preserved
+	it("should have the expected semantic elements", () => {
+		document.body.appendChild(element);
+
+		const headerEl = element.shadowRoot.querySelector("header");
+		expect(headerEl).toBeTruthy();
+
+		const mainEl = element.shadowRoot.querySelector("main");
+		expect(mainEl).toBeTruthy();
+
+		const footerEl = element.shadowRoot.querySelector("footer");
+		expect(footerEl).toBeTruthy();
+	});
+});
