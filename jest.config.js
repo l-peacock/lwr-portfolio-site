50eea046ea4942d651c58b8a0fec6a179ed8ce19
Lachlan Peacock <lachlan.peacock@gmail.com>
2024-02-10 20:11:00 +1100

    Add github workflow to validate pull requests

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,12 @@ export default {
 		"!**/node_modules/**",
 		"!**/vendor/**",
 	],
+	coverageThreshold: {
+		global: {
+			branches: 70,
+			functions: 70,
+			lines: 70,
+			statements: 70,
+		},
+	},
 };
